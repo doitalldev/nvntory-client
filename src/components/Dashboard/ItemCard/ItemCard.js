2a514cdd57e6e285019471e09ada13b073aed4c2
root <root@pop-os.localdomain>
2020-07-01 07:32:37 -0700

    init

--- src/components/Dashboard/ItemCard/ItemCard.js
+++ src/components/Dashboard/ItemCard/ItemCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ItemCard = ({ item }) => {
   console.log(item);
-  const { name, description, price, cost } = item;
+  const { name, description, price, cost, inventory } = item;
 
   return (
     <div className='card'>
@@ -10,7 +10,9 @@ const ItemCard = ({ item }) => {
       <div className='description'>{description}</div>
       <span className='price'>{price}</span>
       <span className='cost'>{cost}</span>
+      <div className='inventory'>{inventory}</div>
       <button type='submit'>Edit</button>
+      <button type='submit'>Delete</button>
     </div>
   );
 };

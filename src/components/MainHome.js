a5b8413d8ce679e25a82259bbf728a9a08b04ee2
Jonathan Clark <jonny@pop-os.localdomain>
2020-07-06 11:23:46 -0700

    adding readme info

--- src/components/MainHome.js
+++ src/components/MainHome.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { HomeHeader } from './Header';
 import './home.css';
-const heroimage = require('../images/hero1.jpg');
 
 const MainHome = () => {
   return (
@@ -12,9 +11,6 @@ const MainHome = () => {
         small to medium sized businesses that need plenty of features to compete
         with the larger corporations.
       </h3>
-      {/* <section className='hero1'>
-        <img src={heroimage} alt='warehouse' />
-      </section> */}
     </>
   );
 };

ecda7680cb1e0cf6ef06f7fce9209ea3df96ed85
Jonathan Clark <jonny@pop-os.localdomain>
2020-07-08 12:15:33 -0700

    init

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -3,28 +3,53 @@ import config from '../config';
 import './additem.css';
 
 const AddItem = () => {
-  const [sku, setSku] = useState('');
+  /* const [sku, setSku] = useState('');
   const [name, setName] = useState('');
   const [description, setDescription] = useState('');
   const [price, setPrice] = useState('');
   const [cost, setCost] = useState('');
-  const [inventory, setInventory] = useState('');
+  const [inventory, setInventory] = useState(''); */
 
-  const onSubmitForm = async (e) => {
+  const onSubmitForm = (e) => {
     e.preventDefault();
-    try {
-      const body = { sku, name, description, price, cost, inventory };
-      await fetch(`${config.API_ENDPOINT}/api/items`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(body),
-      });
+    const newItem = {
+      sku: e.target['sku'].value,
+      name: e.target['name'].value,
+      description: e.target['description'].value,
+      price: e.target['price'].value,
+      cost: e.target['cost'].value,
+      inventory: e.target['inventory'].value,
+    };
 
-      window.location = '/dashboard';
-    } catch (error) {
-      console.error(error.message);
-    }
+    fetch(`${config.API_ENDPOINT}/api/items`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(newItem),
+    }).then((res) => {
+      if (!res.ok) return res.json().then((e) => Promise.reject(e));
+      return res.json();
+    });
   };
+
+  // const onSubmitForm = async (e) => {
+  //   console.log(e);
+
+  //   e.preventDefault();
+  //   try {
+  //     const body = { sku, name, description, price, cost, inventory };
+  //     await fetch(`${config.API_ENDPOINT}/api/items`, {
+  //       method: 'POST',
+  //       headers: { 'Content-Type': 'application/json' },
+  //       body: JSON.stringify(body),
+  //     });
+
+  //     window.location = '/dashboard';
+  //   } catch (error) {
+  //     console.error(error.message);
+  //   }
+  // };
 
   return (
     <>
@@ -36,8 +61,8 @@ const AddItem = () => {
           type='text'
           name='sku'
           id='sku'
-          value={sku}
-          onChange={(e) => setSku(e.target.value)}
+          /*    value={sku}
+          onChange={(e) => setSku(e.target.value)} */
         />
         <label htmlFor='name'>Name: </label>
         <input
@@ -45,8 +70,8 @@ const AddItem = () => {
           type='text'
           name='name'
           id='name'
-          value={name}
-          onChange={(e) => setName(e.target.value)}
+          /*   value={name}
+          onChange={(e) => setName(e.target.value)} */
         />
         <label htmlFor='description'>Description: </label>
         <input
@@ -54,8 +79,8 @@ const AddItem = () => {
           type='text'
           name='description'
           id='description'
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
+          /*  value={description}
+          onChange={(e) => setDescription(e.target.value)} */
         />
         <label htmlFor='price'>Price: </label>
         <input
@@ -63,8 +88,8 @@ const AddItem = () => {
           required
           name='price'
           id='price'
-          value={price}
-          onChange={(e) => setPrice(e.target.value)}
+          /*  value={price}
+          onChange={(e) => setPrice(e.target.value)} */
         />
         <label required htmlFor='cost'>
           Cost:
@@ -74,8 +99,8 @@ const AddItem = () => {
           required
           name='cost'
           id='cost'
-          value={cost}
-          onChange={(e) => setCost(e.target.value)}
+          /*    value={cost}
+          onChange={(e) => setCost(e.target.value)} */
         />
         <label htmlFor='inventory'>Inventory: </label>
         <input
@@ -83,12 +108,14 @@ const AddItem = () => {
           required
           name='inventory'
           id='inventory'
-          value={inventory}
-          onChange={(e) => setInventory(e.target.value)}
+          /*  value={inventory}
+          onChange={(e) => setInventory(e.target.value)} */
         />
         <button type='submit'>Submit</button>
 
-        <button type='button' onClick={() => (window.location = '/dashboard')}>
+        <button
+          type='button' /* onClick={() => (window.location = '/dashboard')} */
+        >
           Cancel
         </button>
       </form>
